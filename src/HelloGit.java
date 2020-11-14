68db24d031903e588b35105fba80796be17ced82
Anna Adamik <quenthel5@gmail.com>
2020-11-14 22:47:39 +0100

    dodanie nowych zmian

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Hello Git");
         }
 
